86670031180b6c97adb64857cb0c3f9d2e0371c2
quynhosuperkid <hoangthibichvan95@gmail.com>
2017-05-12 21:22:55 +0700

    update fix error

--- 1412689-1412624-data.sql
+++ 1412689-1412624-data.sql
@@ -1,146 +1,140 @@
 ------------------DROP---------------------
-Drop table ChiNhanh
-
+/*
+Drop table ChiNhanh;
 DROP TABLE NhanVien CASCADE CONSTRAINTS;
 DROP TABLE ChiNhanh CASCADE CONSTRAINTS;
 DROP TABLE PhongBan CASCADE CONSTRAINTS;
-Drop table NhanVien
+Drop table NhanVien;
+Drop table PhanCong;
+Drop table PhongBan;
+Drop table ChiTieu;
+Drop table DuAn;
+*/
 
-
-Drop table PhanCong
-
-Drop table PhongBan
-
-
-Drop table ChiTieu
-
-
-Drop table DuAn
-
---B?ng Chi Nhánh
-Create table ChiNhanh(
-maCN varchar2(8) NOT NULL,
-tenCN varchar2(50) NOT NULL,
-truongChiNhanh varchar2(8) NOT NULL
-)
+--Bang Chi Nhanh
+CREATE TABLE ChiNhanh(
+maCN VARCHAR2(8) NOT NULL,
+tenCN VARCHAR2(50) NOT NULL,
+truongChiNhanh VARCHAR2(8) NOT NULL
+);
 
 ALTER TABLE ChiNhanh
 ADD CONSTRAINT PK_ChiNhanh
-PRIMARY KEY(maCN)
+PRIMARY KEY(maCN);
 
---B?ng Nhân Viên
-Create table NhanVien(
-maNV varchar2(8) NOT NULL,
-hoTen varchar2(50) NOT NULL,
-diaChi varchar2(100) NOT NULL,
-dienThoai number,
-email varchar2(100),
-maPhong varchar2(8),
-chiNhanh varchar2(8),
-luong float
-)
+--Bang Nhan Vien
+CREATE TABLE NhanVien(
+maNV VARCHAR2(8) NOT NULL,
+hoTen VARCHAR2(50) NOT NULL,
+diaChi VARCHAR2(100) NOT NULL,
+dienThoai VARCHAR2(20),
+email VARCHAR2(100),
+maPhong VARCHAR2(8),
+chiNhanh VARCHAR2(8),
+luong FLOAT
+);
 ALTER TABLE NhanVien
 ADD CONSTRAINT PK_NhanVien
-PRIMARY KEY(maNV)
+PRIMARY KEY(maNV);
 
---B?ng phòng ban
-Create table PhongBan(
-maPhong varchar2(8) NOT NULL,
-tenPhong varchar2(50) NOT NULL,
-truongPhong varchar2(8) NOT NULL,
-ngayNhamChuc varchar2(50),
-soNhanVien number,
-chiNhanh varchar2(8) NOT NULL
-)
+--Bang Phong Ban
+CREATE TABLE PhongBan(
+maPhong VARCHAR2(8) NOT NULL,
+tenPhong VARCHAR2(50) NOT NULL,
+truongPhong VARCHAR2(8) NOT NULL,
+ngayNhamChuc VARCHAR2(50),
+soNhanVien NUMBER,
+chiNhanh VARCHAR2(8) NOT NULL
+);
 ALTER TABLE PhongBan
 ADD CONSTRAINT PK_PhongBan
-PRIMARY KEY(maPhong)
+PRIMARY KEY(maPhong);
 
---B?ng DuAn
-Create table  DuAn(
-maDA varchar2(8) NOT NULL,
-tenDA varchar2(50) NOT NULL,
-kinhPhi float,
-phongChuTri varchar2(8),
-truongDA varchar2(8)
-)
+--Bang Du An
+CREATE TABLE  DuAn(
+maDA VARCHAR2(8) NOT NULL,
+tenDA VARCHAR2(50) NOT NULL,
+kinhPhi FLOAT,
+phongChuTri VARCHAR2(8),
+truongDA VARCHAR2(8)
+);
 ALTER TABLE DuAn
 ADD CONSTRAINT PK_DuAn
-PRIMARY KEY(maDA)
+PRIMARY KEY(maDA);
 
 --B?ng Chi Tieu
-Create table ChiTieu(
-maChiTieu varchar2(8) NOT NULL,
-tenChiTieu varchar2(50) NOT NULL,
-soTien float,
-duAn varchar2(8) NOT NULL
-)
+CREATE TABLE ChiTieu(
+maChiTieu VARCHAR2(8) NOT NULL,
+tenChiTieu VARCHAR2(50) NOT NULL,
+soTien FLOAT,
+duAn VARCHAR2(8) NOT NULL
+);
 ALTER TABLE ChiTieu
 ADD CONSTRAINT PK_ChiTieu
-PRIMARY KEY(maChiTieu)
+PRIMARY KEY(maChiTieu);
 
---B?ng Phân Công
-Create table PhanCong(
-maNV varchar2(8) NOT NULL,
-duAn varchar2(8) NOT NULL,
-vaiTro varchar2(200),
-phuCap float
-)
+--Bang Phan Cong
+CREATE TABLE PhanCong(
+maNV VARCHAR2(8) NOT NULL,
+duAn VARCHAR2(8) NOT NULL,
+vaiTro VARCHAR2(200),
+phuCap FLOAT
+);
 ALTER TABLE PhanCong
 ADD CONSTRAINT PK_PhanCong
-PRIMARY KEY(maNV, duan)
+PRIMARY KEY(maNV, duan);
 ------------------------------------------------
-//--------khóa ngo?i chiNhanh
+--------khoa ngoai ChiNhanh
 ALTER TABLE ChiNhanh
 ADD CONSTRAINT FK1_ChiNhanh_NhanVien 
 FOREIGN KEY(truongChiNhanh) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
-//---------khóa ngo?i NhanVien
+---------khoa ngoai NhanVien
 ALTER TABLE NhanVien
 ADD CONSTRAINT FK1_NhanVien_PhongBan 
 FOREIGN KEY(maPhong) 
-REFERENCES Phongban(maPhong)
+REFERENCES Phongban(maPhong);
 
 ALTER TABLE NhanVien
 ADD CONSTRAINT FK2_NhanVien_ChiNhanh 
 FOREIGN KEY(chiNhanh) 
-REFERENCES ChiNhanh(maCN)
-//------Khóa ngo?i Phòng ban
+REFERENCES ChiNhanh(maCN);
+------Khoa ngoai Phong Ban
 ALTER TABLE Phongban
 ADD CONSTRAINT FK1_PhongBan_ChiNhanh
 FOREIGN KEY(chiNhanh) 
-REFERENCES ChiNhanh(maCN)
+REFERENCES ChiNhanh(maCN);
 
 ALTER TABLE Phongban
 ADD CONSTRAINT FK2_PhongBan_NhanVien 
 FOREIGN KEY(truongPhong) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
-//-------B?ng D? Án
+-------Bang Du An
 ALTER TABLE DuAn
 ADD CONSTRAINT FK1_DuAn_NhanVien
 FOREIGN KEY(truongDA) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
 ALTER TABLE DuAn
 ADD CONSTRAINT FK2_DuAn_PhongBan
 FOREIGN KEY(phongChuTri) 
-REFERENCES Phongban(maPhong)
+REFERENCES Phongban(maPhong);
 
-//------B?ng Chi Tiêu
+------Bang Chi Tieu
 ALTER TABLE ChiTieu
 ADD CONSTRAINT FK1_ChiTieu_DuAn 
 FOREIGN KEY(duAn) 
-REFERENCES DuAn(maDA)
+REFERENCES DuAn(maDA);
 
-//-------B?ng Phân công
+-------Bang Phan Cong
 ALTER TABLE PhanCong
 ADD CONSTRAINT FK1_PhanCong_DuAn 
 FOREIGN KEY(duAn) 
-REFERENCES DuAn(maDA)
+REFERENCES DuAn(maDA);
 
 ALTER TABLE PhanCong
 ADD CONSTRAINT FK2_PhanCong_NhanVien
 FOREIGN KEY(maNV) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
